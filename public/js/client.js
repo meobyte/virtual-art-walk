47aa2fca26ee9b0b5956e5856203367b980db6f0
Michelle Anderson <meobyte@gmail.com>
2016-07-21 14:43:26 -0700

    Add gallery name overlay

--- public/js/client.js
+++ public/js/client.js
@@ -8,18 +8,21 @@
     thisVideoPlayer = videoPlayers[i];
 
     // When enough info about the size has come in, set the container coordinates and width
-    thisVideoPlayer.querySelector(".video__player").addEventListener("loadeddata", function(event){
+    thisVideoPlayer.querySelector(".video__player").addEventListener("loadeddata", function(event) {
       var videoParent = this.parentNode;
-      videoParent.style.left = videoParent.offsetLeft + "px";
-      videoParent.style.top = videoParent.offsetTop + "px";
-      videoParent.style.width = videoParent.offsetWidth+ "px";
+      var videoParentPosition = videoParent.getBoundingClientRect();
+      videoParent.style.left = videoParentPosition.left  + "px";
+      videoParent.style.top = videoParentPosition.top  + "px";
+      videoParent.style.width = videoParent.offsetWidth + "px";
     });
 
     // When a video is clicked toggle fullscreen
     thisVideoPlayer.addEventListener("click", function(event) {
       this.classList.toggle("playing");
       this.classList.add("fixed");
-      if (!this.classList.contains("playing")) {
+      if (this.classList.contains("playing")) {
+        this.querySelector("h1").style.display = "none";
+      } else {
         this.querySelector(".video__overlay").classList.add("hide");
       }
     });
@@ -27,13 +30,15 @@
     // Toggle the overlay
     thisVideoPlayer.addEventListener("transitionend", function(event) {
       var overlay = this.querySelector(".video__overlay");
+
       if (this.classList.contains("playing")) {
         overlay.classList.add("show");
-        overlay.play();
+        this.querySelector(".video__player").play();
       } else {
         this.classList.remove("fixed");
         overlay.classList.remove("show");
         overlay.classList.remove("hide");
+        this.querySelector("h1").style.display = "block";
       }
     });
   }
